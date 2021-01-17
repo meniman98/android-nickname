38416d3574a122ced4dc009d630f495ee8d74644
Huncho Jack <meniman98@gmail.com>
2021-01-17 22:44:21 +0100

    Completed the basic task. Did data binding, added all the views, now theres more to do if you like

--- app/src/main/java/com/example/nickname/MainActivity.kt
+++ app/src/main/java/com/example/nickname/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.nickname
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.databinding.DataBindingUtil
+import com.example.nickname.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // right here, the binding variable is made before the onCreate function
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        // righto, when databinding is used, go ahead and assign the binding variable
+        // to the layout of the xml file. this is the equivalent to using setContentView()
+        // so make sure you remove the setContentView() function
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnDone.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        binding.apply {
+            tvName.text = binding.edName.text
+            // this is used to refresh the UI views
+            invalidateAll()
+            tvName.visibility = VISIBLE
+        }
     }
 }
